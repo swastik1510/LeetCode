1b013f801af419531258340e896d25274a589bcb
swastik <114602590+swastik1510@users.noreply.github.com>
2023-10-21 22:37:23 +0530

    Add files via upload

---
+++ 724.Find_Pivot_Index.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int n = nums.size(); // Get the size of the input array
+        int totalSum = accumulate(nums.begin(), nums.end(), 0); // Compute the sum of all elements in the array
+        int leftSum = 0; // Initialize left sum to 0
+        for(int i = 0; i < n; i++){ // Loop through each index in the array
+            if(leftSum == totalSum - leftSum - nums[i]){ // Check if the left sum equals the right sum
+                return i; // Return the pivot index if found
+            }
+            leftSum += nums[i]; // Update the left sum by adding the current element to it
+        }
+        return -1; // Return -1 if no pivot index is found
+    }
+};
